43d4bee4f333c998b61b5375992bb68b13df1ef7
cristoper <cristoper.runco@banexcoin.com>
2025-03-21 17:44:11 -0500

    feat: enhance search API with response schema and success message

--- src/modules/search/search.controller.ts
+++ src/modules/search/search.controller.ts
@@ -1,5 +1,5 @@
 import { Controller, Get, Query } from '@nestjs/common';
-import { ApiOperation, ApiQuery, ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiQuery, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { SearchService } from './search.service';
 import { SearchDto } from './dto/search.dto';
 
@@ -14,6 +14,17 @@ export class SearchController {
     name: 'documentType',
     enum: ['dni', 'ruc'],
     description: 'Tipo de documento',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Información encontrada',
+    example: {
+      status: 'success',
+      data: {
+        fullName: 'Nombre Apellido / Nombre Empresa',
+        checkDigit: '1',
+      },
+    },
   })
   @ApiQuery({ name: 'documentNumber', description: 'Número de documento' })
   async findAll(@Query() query: SearchDto) {
